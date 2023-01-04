9e27e4d31c0f65ec53e8a87c95772cd678aff7c2
Mykola Shydlovskyi <mikola.shidlovsky@gmail.com>
2023-01-04 13:55:14 +0200

    First commit - Create basic structure - Add get quotes endpoint

---
+++ handlers/getQuotes.js
@@ -0,0 +1,37 @@
+import AWS from 'aws-sdk';
+
+AWS.config.update({region: process.env.REGION})
+
+const s3 = new AWS.S3()
+
+export const getQuotes = async (event, context) => {
+    try {
+        console.log("Incoming:::", event);
+
+        //get json file from s3
+        const bucketData = await s3.getObject({
+                Bucket: "myjsonbucket",
+                Key: "quotes.json"
+            }
+        ).promise()
+
+        const jsonData = JSON.parse(bucketData.Body)
+        console.log("JSON:::", jsonData);
+
+        // create response
+        return {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Methods": "*",
+                "Access-Control-Allow-Origin": "*",
+            },
+            statusCode: 200,
+            body: JSON.stringify(jsonData)
+        }
+
+    } catch (error) {
+        console.error(error.message);
+        return new Error(error.message)
+    }
+
+}
